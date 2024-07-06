6a59023a21094b58f7daf8320f635dc3593292f1
Tanish_Gabani <113323459+Tanishgabani@users.noreply.github.com>
2024-07-06 16:19:03 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://deploy-mern-1whq.vercel.app"],
+    origin: [""],
     methods: ["POST", "GET"],
     credentials: true,
   })
