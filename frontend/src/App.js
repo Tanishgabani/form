a27dfe1efc20a9953f45d8b6c5b1de5949a59e81
Tanishgabani <gabanitanish09@gmail.com>
2024-07-06 17:34:41 +0530

    first

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ function App() {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "https://form-eta-seven.vercel.app/api/user/create-user",
+        "https://form-two-psi.vercel.app/api/user/create-user",
         formData
       );
       setMessage(response.data.message);
