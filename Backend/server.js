e2d4931913d8b309aee379cf7ac3e1f2ca21d9c8
Tanishgabani <gabanitanish09@gmail.com>
2024-07-06 16:08:53 +0530

    recent commit

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,13 @@ dotenv.config();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 // DB Connection
 connectDB();
