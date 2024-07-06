51283047ea950d6ad982fcaac9ff3674049fb984
Tanishgabani <gabanitanish09@gmail.com>
2024-07-06 18:39:02 +0530

    recent

--- Backend/server.js
+++ Backend/server.js
@@ -10,25 +10,13 @@ dotenv.config();
 // Middleware
 app.use(express.json());
 
-// // CORS Configuration
-// app.use(
-//   cors({
-//     origin: ["https://form-87ig.vercel.app/"],
-//     methods: ["POST", "GET", "OPTIONS"],
-//     credentials: true,
-//   })
-// );
-
-// app.options("*", cors());
-
 // DB Connection
-connectDB();
-
-// Routes
 import userRoute from "./Route/user.route.js";
+connectDB().then(() => {
+  // Routes
+  app.use("/api/user", userRoute);
 
-app.use("/api/user", userRoute);
-
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`Server is running at port ${process.env.PORT || 8000}`);
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port ${process.env.PORT || 8000}`);
+  });
 });
